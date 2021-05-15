5c556e08072f4624b2bf125ad8fb4e03d01a1b91
hask777 <hask777@kali.hask777>
2021-05-15 12:15:39 -0400

    first

---
+++ src/brands.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def parse_brands():
+
+    base_url = 'https://av.by/'
+
+    headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+        }
+
+    r = requests.get(base_url)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    brandlist = soup.find('ul', class_='brandslist')
+
+    lis = soup.find_all('li', class_="brandsitem")
+    print(lis)
+
+    brands_dict = {}
+    list_brands = []
+
+    for item in lis:
+        name = item.find('span').text
+        # print(name)
+
+        cars_count = item.find('small').text
+        # print(count)
+        if int(cars_count) > 25:
+            brands_dict = {
+                'name': name.lower(),
+                'cars_count': int(cars_count)
+            }
+        # print(brands_dict)
+            list_brands.append(brands_dict)
+    print(list_brands)
+
+    # brands = "brands.json"
+    # with open(brands, 'w', encoding='utf-8') as json_file:
+    #     json.dump(list_brands, json_file, ensure_ascii = False, indent =4)
+
+    return list_brands
+
+# parse_brands()
